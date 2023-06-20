93ef92bded2e75c4be1034270c92df7e3ad49cdb
vsev <vsevex@gmail.com>
2023-06-20 19:47:41 +0400

    feat(muc): update extension logic

--- lib/extensions/muc/muc_extension.dart
+++ lib/extensions/muc/muc_extension.dart
@@ -2,6 +2,7 @@ import 'package:echo/echo.dart';
 import 'package:echo/src/constants.dart';
 
 part 'room.dart';
+part 'occupant.dart';
 
 class MUCExtension extends Extension {
   MUCExtension() : super('multi-chat user');
@@ -9,6 +10,8 @@ class MUCExtension extends Extension {
   Handler? handler;
   final rooms = <String, Room>{};
 
+  /// Initialize the Multi-User Chat Plugin. Sets connection object and
+  /// extends namespace values.
   @override
   void initialize(Echo echo) {
     super.echo = echo;
@@ -21,16 +24,86 @@ class MUCExtension extends Extension {
       ..addNamespace('MUC_REGISTER', 'jabber:iq:register');
   }
 
+  Future<void> join(
+    String room,
+    String nickname, {
+    String? roomcode,
+    Map<String, String>? historyAttributes,
+    XmlElement? extendedPresence,
+  }) async {
+    final presence = EchoBuilder.pres(
+      attributes: {
+        'from': echo!.jid,
+        'to': _roomNickname(room, nickname: nickname)
+      },
+    ).c('x', attributes: {'xmlns': ns['MUC']!});
+
+    if (historyAttributes != null) {
+      presence.c('history', attributes: historyAttributes).up();
+    }
+
+    if (roomcode != null) {
+      presence.cnode(Echotils.xmlElement('password', text: roomcode)!);
+    }
+
+    if (extendedPresence != null) {
+      presence.up().cnode(extendedPresence);
+    }
+
+    handler ??= echo!.addHandler((stanza) async {
+      final from = stanza.getAttribute('from');
+      if (from == null) {
+        return true;
+      }
+
+      final roomname = from.split('/')[0];
+      if (rooms[roomname] == null) {
+        return true;
+      }
+
+      final room = rooms[roomname];
+      Map<int, Handler> handlers = {};
+      if (stanza.localName == 'message') {
+        handlers = room!._messageHandlers;
+      } else if (stanza.localName == 'presence') {
+        final xquery = stanza.findAllElements('x').toList();
+        if (xquery.isNotEmpty) {
+          for (int i = 0; i < xquery.length; i++) {
+            final x = xquery[i];
+            final xmlns = x.getAttribute('xmlns');
+            if (xmlns != null && xmlns.contains(ns['MUC']!)) {
+              handlers = room!._presenceHandlers;
+            }
+          }
+        }
+      }
+      for (final entry in handlers.entries) {
+        final handler = entry.value;
+        if (!await handler.run(stanza)!) {
+          handlers.removeWhere((key, value) => key == entry.key);
+        }
+      }
+      return true;
+    });
+    if (!rooms.containsKey(room)) {
+      rooms[room] = Room();
+    }
+
+    await echo!.send(presence);
+  }
+
   Future<void> queryOccupants(String room) {
     final info = EchoBuilder.iq(
       attributes: {'type': 'get', 'from': echo!.jid, 'to': room},
     ).c('query', attributes: {'xmlns': ns['DISCO_ITEMS']!});
 
     return echo!.sendIQ(
-        element: info.nodeTree!,
-        callback: (element) {
-          print(element);
-        });
+      element: info.nodeTree!,
+      callback: (element) {
+        print(element);
+      },
+      onError: (error) => print('error: $error'),
+    );
   }
 
   Future<void> createInstantRoom(String room) => super.echo!.sendIQ(
@@ -55,79 +128,11 @@ class MUCExtension extends Extension {
         print('error:$error');
       });
 
-  // Future<void> join(
-  //   String room,
-  //   String nickname, {
-  //   String? roomcode,
-  //   Map<String, String>? historyAttributes,
-  //   XmlElement? extendedPresence,
-  // }) async {
-  //   final presence = EchoBuilder.pres(
-  //     attributes: {
-  //       'from': echo!.jid,
-  //       'to': _roomNickname(room, nickname: nickname)
-  //     },
-  //   ).c('x', attributes: {'xmlns': ns['MUC']!});
-
-  //   if (historyAttributes != null) {
-  //     presence.c('history', attributes: historyAttributes).up();
-  //   }
-
-  //   if (roomcode != null) {
-  //     presence.cnode(Echotils.xmlElement('password', text: roomcode)!);
-  //   }
-
-  //   if (extendedPresence != null) {
-  //     presence.up().cnode(extendedPresence);
-  //   }
-
-  //   handler = Handler((stanza) async {
-  //     final from = stanza.getAttribute('from');
-  //     if (from == null) {
-  //       return true;
-  //     }
-
-  //     final roomname = from.split('/')[0];
-
-  //     if (!rooms.containsKey(roomname)) {
-  //       return true;
-  //     }
-
-  //     final room = rooms[roomname];
-  //     Map<String, Handler> handlers = <String, Handler>{};
-
-  //     if (stanza.localName == 'message') {
-  //       handlers = room!.messageHandlers;
-  //     } else if (stanza.localName == 'presence') {
-  //       final xQuery = stanza.findAllElements('x');
-
-  //       for (final x in xQuery) {
-  //         final xmlns = x.getAttribute('xnlns');
-  //         if (xmlns != null && xmlns.contains(ns['MUC']!)) {
-  //           handlers = room!.presenceHandlers;
-  //           break;
-  //         }
-  //       }
-  //     }
-
-  //     handlers.entries.map((item) async {
-  //       if (!await item.value.handler!(stanza)) {
-  //         handlers.remove(item.key);
-  //       }
-  //     });
-
-  //     if (!rooms.containsValue(room)) {
-
-  //     }
-  //   });
-  // }
-
   String _roomNickname(String room, {String? nickname}) {
     final node = Echotils.escapeNode(Echotils().getNodeFromJID(room)!);
-    final domain = Echotils().getDomainFromJID(node);
+    final domain = Echotils().getDomainFromJID(room);
+    final suffix = nickname != null ? '/$nickname' : '';
 
-    final suffix = nickname != null ? '#/$nickname' : '';
-
-    return '$node@$domain $suffix';
+    return '$node@$domain$suffix';
   }
 }
